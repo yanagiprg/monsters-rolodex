941041e65709c77731afba998e0704ddaf42a389
yanagi_prg <takayanagi.prg@gmail.com>
2019-08-18 12:54:26 +0900

    Updates

---
+++ precache-manifest.7972c994dc963a14f87c4e62ff877eb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6483e8e41815eee9789133cae626cab2",
+    "url": "/udemy_react/index.html"
+  },
+  {
+    "revision": "15f78ddc070e38142d94",
+    "url": "/udemy_react/static/css/main.b14c0e6e.chunk.css"
+  },
+  {
+    "revision": "350aa9b83f1e03a99420",
+    "url": "/udemy_react/static/js/2.e9fbb397.chunk.js"
+  },
+  {
+    "revision": "15f78ddc070e38142d94",
+    "url": "/udemy_react/static/js/main.26e448c1.chunk.js"
+  },
+  {
+    "revision": "62504d3807bfbc004815",
+    "url": "/udemy_react/static/js/runtime~main.1af17540.js"
+  }
+]);
